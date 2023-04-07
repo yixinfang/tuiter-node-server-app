247f994f5f00b05cdfa5ed16a909d9cda7595621
yixinfang <fangyix@northeastern.edu>
2023-04-06 21:37:09 -0400

    a9 Mongo sp23

---
+++ controllers/tuits/tuits-schema.js
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+const schema = mongoose.Schema({
+    topic: String,
+    username: String,
+    handle: String,
+    time: String,
+    image: String,
+    title: String,
+    tuit: String,
+    likes: Number,
+    liked: Boolean,
+    dislikes: Number,
+    replies: Number,
+    retuits: Number,
+}, {collection: 'tuits'});
+export default schema;
